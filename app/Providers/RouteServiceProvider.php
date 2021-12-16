767ba9b24b608519c0f6c2a5ee0ca949190e3fed
ChazyTheBest <chazythebest@hotmail.es>
2021-12-16 20:52:57 +0100

    fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const HOME = '';
+    const HOME = '/';
 
     /**
      * The controller namespace for the application.
