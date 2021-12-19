ac1b13318f846a56efc532a0acdc3f36a046b9b4
ChazyTheBest <chazythebest@hotmail.es>
2021-12-19 13:22:25 +0100

    fix test

--- routes/www.php
+++ routes/www.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebController;
 use App\Http\Controllers\ShopController;
-use App\Http\Models\Presale;
+use App\Models\Presale;
 
 Route::domain('www.' . config('app.domain'))->group(function ()
 {
