a2b5583c267d59d83a7dbe5182e02cc5fafe040c
ChazyTheBest <chazythebest@hotmail.es>
2021-12-22 05:00:24 +0100

    test stuff

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -106,11 +106,10 @@ class GameController extends Controller
         if ($payment->doesntExist())
             abort(404, "The Purchase doesn't exist.");
 
-        $character = $payment->first()->character;
-        if ($character->doesntExist())
+        if ($payment->character->doesntExist())
             abort(404, "The Character doesn't exist.");
 
-        //$character = $character->first();
+        $character = $payment->first()->character;
 
         $training = $character->latestTraining();
         if ($training->exists() && !$training->done)
