6abbb0339afbb52cce846ed1fe1056d4ee91c3b5
ChazyTheBest <chazythebest@hotmail.es>
2021-12-23 17:40:41 +0100

    moralis wip

--- public_static/js/wallet.js
+++ public_static/js/wallet.js
@@ -4,7 +4,7 @@ Moralis.start({ serverUrl: 'https://nzcmuvh8i6vd.usemoralis.com:2053/server', ap
 const tokenAddress = '0xbf4013ca1d3d34873a3f02b5d169e593185b0204',
       goalBalance = document.getElementById('goal-balance');
 
-let user = null, provider = 'metamask';
+let user = Moralis.User.current(), provider = 'metamask';
 
 /* Authentication code */
 async function login() {
@@ -12,12 +12,7 @@ async function login() {
     if (!user) {
         user = await Moralis.authenticate({ provider: provider, signingMessage: 'Connect to GoaL StadiuM' })
             .then(function (user) {
-                Moralis.Web3API.account.getTokenBalances({ chain: 'bsc' }).forEach(function(token) {
-                    if (token.token_address === tokenAddress)
-                    {
-                        goalBalance.textContent = Number.parseFloat(balance / parseInt('1'.padEnd(token.decimals + 1, '0'))).toFixed(4);
-                    }
-                });
+                updateBalance();
             })
             .catch(function (error) { console.log(error); });
     }
@@ -36,6 +31,18 @@ function walletconnect() {
     provider = 'walletconnect';
     login();
 }
+async function updateBalance()
+{
+    await Moralis.Web3API.account.getTokenBalances({ chain: 'bsc' }).forEach(function(token) {
+        if (token.token_address === tokenAddress)
+        {
+            goalBalance.textContent = Number.parseFloat(balance / parseInt('1'.padEnd(token.decimals + 1, '0'))).toFixed(4);
+        }
+    });
+}
+
+if (user)
+    updateBalance();
 
 document.getElementById('btn-login-metamask').onclick = metamask;
 document.getElementById('btn-login-walletconnect').onclick = walletconnect;
