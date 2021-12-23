59e87dac92e4cc0d390a19a1dd7efa21fedd9111
ChazyTheBest <chazythebest@hotmail.es>
2021-12-23 17:38:16 +0100

    moralis wip

--- public_static/js/wallet.js
+++ public_static/js/wallet.js
@@ -1,5 +1,5 @@
 /* Moralis init code */
-Moralis.start({ serverUrl: 'https://9iuhdje4owkr.usemoralis.com:2053/server', appId: 'DTD1H8BKuE2sSM34ppMhV5IlG0DYROpsJRHmnYKl' });
+Moralis.start({ serverUrl: 'https://nzcmuvh8i6vd.usemoralis.com:2053/server', appId: 'HUsP6xB3wTQTddinmpkkdW71UZ8x8fI0rJs8wOF4' });
 
 const tokenAddress = '0xbf4013ca1d3d34873a3f02b5d169e593185b0204',
       goalBalance = document.getElementById('goal-balance');
