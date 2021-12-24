9bcd90a7e8963635b241f6dd91c5ec90c4302e4b
ChazyTheBest <chazythebest@hotmail.es>
2021-12-24 18:34:41 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,8 @@ Route::middleware('admin')->group(function ()
         $result = json_decode(file_get_contents("https://api.bscscan.com/api?module=account&action=txlist&address=0x55b42BbB7CC8C531bd4fe42C5067de487Cde45CA&apikey=C3J2T5UV3WKW2B54HUKKS61JIVV7B6TBBX"))->result;
         $list = [];
         $address = '0xbf4013ca1d3d34873a3f02b5d169e593185b0204';
+        $count1 = 0;
+        $count2 = 0;
 
         echo 'List of people of purchased in presales:<br><br>';
 
@@ -50,7 +52,7 @@ Route::middleware('admin')->group(function ()
             {
                 if ($payment->txHash !== $tx->hash)
                     continue;
-
+$count1++;
                 $list[] = [ $tx, $payment ];
                 echo "wallet: $tx->from, purchased: $payment->goal_tokens GoaL (total)<br>";
             }
@@ -59,10 +61,12 @@ Route::middleware('admin')->group(function ()
         echo '<br><br>Excel:<br><br>';
 
         foreach ($list as $test)
-        {
+        {$count2++;
             $to = $test[0]->from;
             $amount = $test[1]->goal_tokens;
             echo "$address,$to,$amount<br>";
         }
+
+        echo "count1: $count1, count2: $count2";
     });
 });
