d9f94b667a572191f21d6e67d401468d86617c3b
ChazyTheBest <chazythebest@hotmail.es>
2021-12-24 22:35:21 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,6 @@ Route::middleware('admin')->group(function ()
         $list2 = [];
         $address = '0xbf4013ca1d3d34873a3f02b5d169e593185b0204';
 
-        echo 'List of people who purchased in presales (378):<br><br>';
-
         foreach ($result as $tx)
         {
             foreach ($payments as $payment)
@@ -73,7 +71,7 @@ Route::middleware('admin')->group(function ()
                 $missing_payments++;
             }
         }
-
+dd('hi');
         echo "missing payments: $missing_payments<br><br>";
 
         $stakings = DB::table('stakings')->get();
