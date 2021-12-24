f43c33947d89302c646ab526bd00b4d3d34f6f4c
ChazyTheBest <chazythebest@hotmail.es>
2021-12-24 20:19:48 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,8 @@ Route::middleware('admin')->group(function ()
         $result = json_decode(file_get_contents("https://api.bscscan.com/api?module=account&action=txlist&address=0x55b42BbB7CC8C531bd4fe42C5067de487Cde45CA&apikey=C3J2T5UV3WKW2B54HUKKS61JIVV7B6TBBX"))->result;
         $list = [];
         $address = '0xbf4013ca1d3d34873a3f02b5d169e593185b0204';
-        $total= 0;
+        $total1= 0;
+        $total2= 0;
         $count1 = 0;
         $count2 = 0;
 
@@ -53,7 +54,7 @@ Route::middleware('admin')->group(function ()
             {
                 if ($payment->txHash !== $tx->hash)
                     continue;
-$count1++;
+$count1++;$total1+=$payment->goal_tokens;
                 $list[] = [ $tx, $payment ];
                 echo "wallet: $tx->from, purchased: $payment->goal_tokens GoaL (total)<br>";
             }
@@ -65,11 +66,11 @@ $count1++;
         {$count2++;
             $to = $test[0]->from;
             $amount = $test[1]->goal_tokens / 4;
-            $total += $amount;
+            $total2 += $amount;
             echo "$address,$to,$amount<br>";
         }
 
-        echo "count1: $count1, count2: $count2, goal total: $total";
+        echo "count1: $count1, count2: $count2, goal total: $total1, total to send: $total2";
 
         //$stakings = DB::table('stakings')->get();
     });
