46e2a6ffaa8739b4f6b1e3a94f0f78575c2432bd
ChazyTheBest <chazythebest@hotmail.es>
2021-12-24 01:57:55 +0100

    moralis wip

--- public_static/js/shop.js
+++ public_static/js/shop.js
@@ -98,7 +98,7 @@ async function purchase(product)
 {
     const goal = (await fetchToken()).data,
           decimals = (await Moralis.Web3API.token.getTokenMetadata({ chain: 'bsc', addresses: tokenAddress }))[0].decimals;
-console.log(product.dataset.price);
+console.log(product);
     await Moralis.enableWeb3();
 
     let transferResult = await Moralis.transfer({
