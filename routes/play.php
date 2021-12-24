2a50e4407428c4c03e8dacd6e7250a25482b9df1
ChazyTheBest <chazythebest@hotmail.es>
2021-12-24 12:11:34 +0100

    test shop purchase

--- routes/play.php
+++ routes/play.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 
 Route::domain('play.' . config('app.domain'))->group(function ()
 {
-    if (Auth::user()->id === 2)
+    if (optional(Auth::user())->id === 2)
     {
         Route::get('/penalties', [ GameController::class, 'menu' ])->name('menu')->middleware(['auth']);
         Route::get('/penalties/shop', [ ShopController::class, 'shop' ])->name('shop')->middleware(['auth']);
