45a2474a1e838db223c3caad961e6609ae4308a0
ChazyTheBest <chazythebest@hotmail.es>
2021-12-28 22:44:24 +0100

    disable shop

--- routes/play.php
+++ routes/play.php
@@ -8,8 +8,8 @@ Route::domain('play.' . config('app.domain'))->group(function ()
 {
     Route::middleware('auth')->group(function () {
         Route::get('/penalties', [ GameController::class, 'menu' ])->name('menu');
-        Route::get('/penalties/shop', [ ShopController::class, 'shop' ])->name('shop');
-        Route::post('/penalties/shop/purchase', [ ShopController::class, 'purchase' ]);
+        Route::get('/penalties/shop', [ ShopController::class, 'shop' ])->name('shop')->middleware('admin');
+        Route::post('/penalties/shop/purchase', [ ShopController::class, 'purchase' ])->middleware('admin');
         Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ]);
     });
 });
