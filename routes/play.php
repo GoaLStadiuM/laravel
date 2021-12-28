a839e5e4189d941c0ad5c50ce7664f28a3325324
ChazyTheBest <chazythebest@hotmail.es>
2021-12-28 18:53:31 +0100

    disable throttle for the time being

--- routes/play.php
+++ routes/play.php
@@ -10,6 +10,6 @@ Route::domain('play.' . config('app.domain'))->group(function ()
         Route::get('/penalties', [ GameController::class, 'menu' ])->name('menu');
         Route::get('/penalties/shop', [ ShopController::class, 'shop' ])->name('shop');
         Route::post('/penalties/shop/purchase', [ ShopController::class, 'purchase' ]);
-        Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ])->middleware('throttle:1,1');
+        Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ]);
     });
 });
