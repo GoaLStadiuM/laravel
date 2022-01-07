76044a90c5a0ae87c8b44775a49b98ca9d59a26e
ChazyTheBest <chazythebest@hotmail.es>
2022-01-07 16:39:03 +0100

    fix route

--- routes/play.php
+++ routes/play.php
@@ -12,6 +12,6 @@ Route::domain('play.' . config('app.domain'))->group(function ()
         Route::get('/penalties/shop', [ ShopController::class, 'shop' ])->name('shop');
         Route::post('/penalties/shop/purchase', [ ShopController::class, 'purchase' ]);
         Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ]);
-        Route::get('/penalties/characterlist', GameController::class, 'characterList');
+        Route::get('/penalties/characterlist', [ GameController::class, 'characterList' ]);
     });
 });
