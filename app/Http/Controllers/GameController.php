b46ee05e584726df880c27b4915f0b925ed53f97
ChazyTheBest <chazythebest@hotmail.es>
2022-01-12 21:51:01 +0100

    still wip

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -75,9 +75,9 @@ class GameController extends Controller
                                 DB::raw('kicks_per_division.kicks - COUNT(`kick`.reward) as kicks_left')
                             )
                             ->groupBy('character.id', 'kicks_per_division.kicks')
-                            ->having('kicks_left', '>', '-1')->toSql()
-                            //->get()
-                            //->pluck('kicks_left', 'id')
+                            ->having('kicks_left', '>', '-1')
+                            ->get()
+                            ->pluck('kicks_left', 'id')
             ]
         ]);
     }
