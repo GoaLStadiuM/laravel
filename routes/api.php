f5671d17871c1f8e9cf1a78612c36c4c08b93002
ChazyTheBest <chazythebest@hotmail.es>
2022-01-19 16:37:56 +0100

    fix api auth login token

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\ShopController;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,27 +20,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/sanctum/token', function (Request $request): JsonResponse
+Route::domain('auth.' . config('app.domain'))->group(function ()
 {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => [ 'The provided credentials are incorrect.' ],
+    Route::post('/sanctum/token', function (Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
         ]);
-    }
 
-    return response([
-        'user' => $user,
-        'token' => $user->createToken($request->device_name)->plainTextToken
-    ], Response::HTTP_CREATED);
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => [ 'The provided credentials are incorrect.' ],
+            ]);
+        }
+
+        return response([
+            'user' => $user,
+            'token' => $user->createToken($request->device_name)->plainTextToken
+        ], Response::HTTP_CREATED);
+    });
+
+    Route::post('/sanctum/token/revoke', fn() => Auth::user()->currentAccessToken()->delete())
+            ->middleware('auth:sanctum');
 });
 
-Route::post('/sanctum/token/revoke', fn() => Auth::user()->currentAccessToken()->delete())
-        ->middleware('auth:sanctum');
+Route::domain('play.' . config('app.domain'))->middleware('auth:sanctum')->group(function ()
+{
+    Route::get('/penalties/play', [ GameController::class, 'play' ]);
+    Route::get('/penalties/characterlist', [ GameController::class, 'characterList' ]);
+    Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ]);
+    Route::get('/penalties/kick/reward/{character_id}', [ GameController::class, 'reward' ]);
+});
--- routes/play.php
+++ routes/play.php
@@ -4,25 +4,14 @@ use App\Http\Controllers\GameController;
 use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
-Route::domain('play.' . config('app.domain'))->group(function ()
+Route::domain('play.' . config('app.domain'))->middleware('verified')->group(function ()
 {
-    Route::middleware('verified')->group(function ()
-    {
-        Route::get('/penalties', [ GameController::class, 'menu' ])->name('menu');
-        Route::get('/penalties/shop', [ ShopController::class, 'shop' ])->name('shop');
-        Route::post('/penalties/shop/purchase', [ ShopController::class, 'purchase' ]);
-        /* tmp routes for testing
-        Route::get('/penalties/play', [ GameController::class, 'play' ]);
-        Route::get('/penalties/characterlist', [ GameController::class, 'characterList' ]);
-        Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ]);
-        Route::get('/penalties/kick/reward/{character_id}', [ GameController::class, 'reward' ]);*/
-    });
-
-    Route::middleware('auth:sanctum')->group(function()
-    {
-        Route::get('/penalties/play', [ GameController::class, 'play' ]);
-        Route::get('/penalties/characterlist', [ GameController::class, 'characterList' ]);
-        Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ]);
-        Route::get('/penalties/kick/reward/{character_id}', [ GameController::class, 'reward' ]);
-    });
+    Route::get('/penalties', [ GameController::class, 'menu' ])->name('menu');
+    Route::get('/penalties/shop', [ ShopController::class, 'shop' ])->name('shop');
+    Route::post('/penalties/shop/purchase', [ ShopController::class, 'purchase' ]);
+    /* tmp routes for testing
+    Route::get('/penalties/play', [ GameController::class, 'play' ]);
+    Route::get('/penalties/characterlist', [ GameController::class, 'characterList' ]);
+    Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ]);
+    Route::get('/penalties/kick/reward/{character_id}', [ GameController::class, 'reward' ]);*/
 });
