e6d0b221214ee9ec49be6d862fa297bbd7503bf3
ChazyTheBest <chazythebest@hotmail.es>
2022-01-23 21:34:05 +0100

    testing routes for admin only

--- routes/play.php
+++ routes/play.php
@@ -10,8 +10,10 @@ Route::domain('play.' . config('app.domain'))->middleware(['verified'])->group(f
     Route::get('/penalties/shop', [ ShopController::class, 'shop' ])->name('shop');
     Route::post('/penalties/shop/purchase', [ ShopController::class, 'purchase' ]);
     /* tmp routes for testing*/
-    Route::get('/penalties/play', [ GameController::class, 'play' ]);
-    Route::get('/penalties/characterlist', [ GameController::class, 'characterList' ]);
-    Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ]);
-    Route::get('/penalties/kick/reward/{character_id}', [ GameController::class, 'reward' ]);
+    Route::middleware('admin')->group(function () {
+        Route::get('/penalties/play', [ GameController::class, 'play' ]);
+        Route::get('/penalties/characterlist', [ GameController::class, 'characterList' ]);
+        Route::get('/penalties/kick/{character_id}', [ GameController::class, 'kick' ]);
+        Route::get('/penalties/kick/reward/{character_id}', [ GameController::class, 'reward' ]);
+    });
 });
