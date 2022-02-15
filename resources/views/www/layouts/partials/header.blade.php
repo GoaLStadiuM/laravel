5c8e63dbdddd7e55651246fdb317c02cbd8c9c20
ChazyTheBest <ChazyTheest@hotmail.es>
2022-02-15 20:51:19 +0100

    .

--- resources/views/www/layouts/partials/header.blade.php
+++ resources/views/www/layouts/partials/header.blade.php
@@ -28,7 +28,7 @@
                                     <form method="POST" action="{{ route('logout') }}">
                                         @csrf
 
-                                        <a href="{{ route('logout') }}">Logout</a>
+                                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
                                     </form>
                                 </li>
                             </ul>
