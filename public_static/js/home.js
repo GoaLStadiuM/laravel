192469ac3a1aa0f3eb597e4abf96a0132d0ae362
ChazyTheBest <ChazyTheest@hotmail.es>
2022-02-17 18:13:21 +0100

    home/nft: play/pause video on mouseover/out

--- public_static/js/home.js
+++ public_static/js/home.js
@@ -1,6 +1,10 @@
 const mailInfo = document.getElementById('mail-info'),
       mailInfl = document.getElementById('mail-infl'),
-      mailInve = document.getElementById('mail-inve');
+      mailInve = document.getElementById('mail-inve'),
+      nftBenja = document.getElementById('nft-benja'),
+      nftCrisnaldo = document.getElementById('nft-crisnaldo'),
+      nftPeque = document.getElementById('nft-peque'),
+      nftSinedine = document.getElementById('nft-sinedine');
 
 mailInfo.textContent = 'info@goalstadium.com';
 mailInfl.textContent = 'influencers@goalstadium.com';
@@ -9,3 +13,12 @@ mailInve.textContent = 'investments@goalstadium.com';
 mailInfo.href = 'mailto:info@goalstadium.com';
 mailInfl.href = 'mailto:influencers@goalstadium.com';
 mailInve.href = 'mailto:investments@goalstadium.com';
+
+nftBenja.addEventListener('mouseover', () => nftBenja.play());
+nftBenja.addEventListener('mouseout', () => nftBenja.pause());
+nftCrisnaldo.addEventListener('mouseover', () => nftCrisnaldo.play());
+nftCrisnaldo.addEventListener('mouseout', () => nftCrisnaldo.pause());
+nftPeque.addEventListener('mouseover', () => nftPeque.play());
+nftPeque.addEventListener('mouseout', () => nftPeque.pause());
+nftSinedine.addEventListener('mouseover', () => nftSinedine.play());
+nftSinedine.addEventListener('mouseout', () => nftSinedine.pause());
